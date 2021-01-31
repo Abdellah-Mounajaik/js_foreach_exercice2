5a17e2b56e1fb910de940073c443a47a41485bdb
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 01:22:43 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+// exo1
+
+// let tab = ["pomme", "fraise", "orange", "melon", "poire"]
+// tab.forEach(element => {
+//     console.log(element);
+// });
+
+
+// exo 2
+
+// let coding16 = ["saliou", "zakaria", "ayoub", "mohammed", "alex", "adame", "sebastien", "achraf", "bene", "abdellah", "malo", "louise", "natchez", "dawid", "maximilien", "jamila", "maxence", "zulma"]
+// coding16.forEach((element, i, coding16) => {
+//     console.log(`Bonjour, ${element} ${i}`);
+// });
